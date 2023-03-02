33b531a31c11412710a2654d764ee9cab359d433
ron hodadedi <hodadedi@g.jct.ac.il>
2023-03-02 12:46:26 +0200

    a

--- Client/SingIn/js/SingInPage.js
+++ Client/SingIn/js/SingInPage.js
@@ -12,18 +12,18 @@ function remcl(){
 	}
 }
 
-function checkInputValue(inputValue) {
-	fetch("../DB/Administrators.json")
-	.then(response => response.json())
-	.then(data => {
-		if (data.Administrators.some(item => item.UserName === inputValue.username && item.Password === inputValue.password)) {
-			console.log("Username and password match found in JSON data");
-			alert("Login successful!");
-		} else {
-			console.log("Username and password not found in JSON data");
-		}
-	})
-	.catch(error => console.error(error));
+function logIn() {
+    let flag = validateFildes();
+    if (flag == true) {
+        let userLoggedIn = {
+            "loggedIn": true,
+            "username": enteredUsername = document.getElementsByName("username")[0].value
+        }
+        localStorage.setItem("userLoggedIn", JSON.stringify(userLoggedIn));
+        window.location.assign("../home/home.html");
+    } else {
+        alert("wrong username or passeord");
+    }
 }
 
 inputs.forEach(input => {
@@ -33,21 +33,8 @@ inputs.forEach(input => {
 		const inputValue = {
 			username: document.getElementById("username").value,
 			password: document.getElementById("password").value
-		};
-		checkInputValue(inputValue);
-		alert("Login !");
+		};		
 	});
 });
 
-document.getElementById("myButton").onclick = function() {
-  // Use AJAX to load the content of the other page
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("contentR").innerHTML = this.responseText;
-    }
-  };
-  xhr.open("GET", "../Client/ApplicationScreen/ApplicationScreen.html", true);
-  xhr.send();
-};
 

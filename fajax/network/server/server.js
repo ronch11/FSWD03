692e3d2c6b7fd8db31374b0b9d0cdfdf97a3c441
uditra <yehudaar@g.jct.ac.il>
2023-03-13 12:53:10 +0200

    server finished for know

--- fajax/network/server/server.js
+++ fajax/network/server/server.js
@@ -8,99 +8,73 @@ class server{
    * @param onReady the call back function 
    */
   UseServer = (FXMLhttpRequest, onReady) => {
-    var resource = FXMLhttpRequest.url
+    var url = FXMLhttpRequest.url
     var body = FXMLhttpRequest.body
     var method = FXMLhttpRequest.method
     var response = {}
-
     switch(method){
       case 'GET':
       {
-        if(resource === "/SignIn"){
-          var user = Database.getUser(body.username, body.password)
+        if(url === "/SignIn"){
+          var user = restAPI.getAdminData(body.username)
           // if user exists
           if (user !== null) {
-              console.log('user exists', user)
-              response = {status: 200,
-                  user: user}
-                  onReady(response);
+              response = { status: 200, body: user }
           } else {
-              console.log('server: user does not exist')
-              response = {status: 404,
-                  user: null}
-                  onReady(response);
+              response = { status: 404, body: null }
           }
           // call onReady when done
         } else {
-          var Tasks = Database.getMissions(body.userId);
-          if (Tasks !== null) {
-              var tasks_list = []
-              for (var i = 0; i < Tasks.length; i++){
-                  tasks_list.push({title : Tasks[i].text, id : Tasks[i].id, done : Tasks[i].done});
-              }
-              response = {status: 200, userId : body.userId,
-                  tasks : tasks_list }
-                  onReady(response);
+          if (body === null) {
+            var books =restAPI.getAllBookData()
+            if (books)
+              response = { status: 200, body: books }
+            else 
+              response = { status: 404, body: null }
+          } else {
+            var book = restAPI.getBookData(body.book_name)
+            if (book)
+              response = { status: 200, body: book }
+            else 
+              response = { status: 404, body: null }
           }
-          // call onReady when done
         }
+        onReady(response)
       }
       case 'POST':
       {
-        if(resource === "/SignUp"){
-          var user = Database.getUser(body.username, body.password)
-          // if user exists
-          if (user !== null) {
+        if(url === "/SignUp"){
+          var user = restAPI.getAdminData(body.username)
+          if (user !== null) { // if user exists
               console.log('user already exists')
-              response = {status: 404,
-                  user: null}
+              response = { status: 404 , body: null }
           } else {
-              console.log('user does not exist, signing up')
-              var user = new User(body.username, body.password, body.fname, body.lname);
-              Database.addUser(user);
-              var user = Database.getUser(body.username, body.password)
-              console.log(user)
-              response = {status: 200,
-                  user: {username : user.username,
-                      password : user.password,
-                        id : user.id,
-                        fname : user.fname,
-                          lname : user.lname}}
-          }
-          // call onReady when done
-          onReady(response);
-        } else {
-          var mission = Database.addMission(body.userId, body.title);
-          if (mission !== null) {
-              response = {status: 200, userId : body.userId,
-                  task : {title : mission.text, id : mission.id, done : mission.done} }
-          }else{
-              response = {status: 404, userId : body.userId,
-                  task : null }
-          }
-          onReady(response);
+            var user = restAPI.createAdminData(body.data)
+            response = { status: 200 , user: user }
         }
+        } else {
+          var book = restAPI.createBookData(body.data)
+          response = { status: 200, body: book }
+        }
+        onReady(response)
       }
       case 'PUT':
       {
-        var mission = Database.restoreMission(body.taskId);
-        if (mission !== null) {
-          response = {status: 200, userId : body.userId, task : {title : mission.text, id : mission.id, done : mission.done} }
-        } else {
-            response = {status: 404, userId : body.userId, task : null }
-        }
-        onReady(response);
+        var book = restAPI.updateBookData(body.book_name, body.data)
+        if (book)
+        response = { status: 200, body: book }
+        else 
+        response = { status: 404, body: null }
+        onReady(response)
       }
       case 'DELETE':
       {
-        var mission = Database.removeMission(body.taskId);
-        if (mission !== null) {
-          response = {status: 200, task : {title : mission.title, id : mission.id, done : mission.done} }
-        }
-        else {
-          response = {status: 404 , task : null }
-        }
-        onReady(response);
+        var books = restAPI.deleteBookData(book_name)
+        if (books)
+          response = { status: 200, body: books }
+        else 
+          response = { status: 404, body: null }
+        onReady(response)
       }
     }
   }

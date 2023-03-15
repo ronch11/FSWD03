b8fbecacb016c7e490aa464f1465070be3e21e72
ron hodadedi <hodadedi@g.jct.ac.il>
2023-03-15 18:15:30 +0200

    SPA WORK

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
-import { createBookData, getBookData, getAllBookData, updateBookData, deleteBookData, getAdminData,createAdminData } from "../../rest api/restApi.js";  
-const { createBookData, getBookData, getAllBookData, updateBookData, deleteBookData, getAdminData,createAdminData } = require('../../rest api/restApi.js');
-
-import {FXMLhttpRequest} from "/fajax/FXMLhttpRequest.js";
-
 
 document.getElementById('LogInButton').addEventListener('click', sign_in);
-document.getElementById('LogInPageButton').addEventListener('click', Getsinup);
+document.getElementById('LogInPageButton').addEventListener('click', sign_in_page);
 document.getElementById('SignUpButton').addEventListener('click', sign_up);
-document.getElementById('SignUpPageButton').addEventListener('click', sign_up_page);
+document.getElementById('SignUpPageButton').addEventListener('click', GetSingUpage);
 
-/*get login page and remove the singup page */
-function Getsinup(){
-    Uploudelogin(); // show login form
-    Removesignup();// remove signup form
+
+
+
+/* get sign in page */
+function sign_in_page(){
+    LoadSinginPage(); // call function to load login page
+    RemoveSignUpPage(); // call function to remove signup page
 }
 
-function Uploudelogin(){
-    const taskTemplate = document.getElementById('login-form');
-    // add hide class to login form
-    taskTemplate.classList.add('display-section');// add display-section class to login form
-    taskTemplate.classList.remove('area');// remove area class from login form
+/* load login page */
+function LoadSinginPage(){
+    const taskTemplate = document.getElementById('login-form');// get login form    
+    taskTemplate.classList.add('display-section');// add display class to login form
+    taskTemplate.classList.remove('area');// remove hide class to login form
 }
 
-function Removesignup(){
-    const taskTemplate = document.getElementById('signup-form');
-    // add hide class to login form
-    taskTemplate.classList.add('area');// add area class to login form
-    taskTemplate.classList.remove('display-section'); // remove display-section class from login form
+/* remove signup page */
+function RemoveSignUpPage(){
+    const taskTemplate = document.getElementById('signup-form');// get signup form
+    taskTemplate.classList.add('area');// add hide class to login form
+    taskTemplate.classList.remove('display-section');// remove display class to login form
 }
 
-
-/*get signup page and remove the login page */
-function sign_up_page(){
-    Uploudesignup();// show signup form
-    Removelogin();// remove login form
+/* get sign up page */
+function GetSingUpage(){
+    LoadSingUPage();
+    RemoveSignInPage();
 }
 
-function Uploudesignup(){
-    const taskTemplate = document.getElementById('signup-form');
-    // add hide class to login form
-    taskTemplate.classList.add('display-section');// add display-section class to login form
-    taskTemplate.classList.remove('area');// remove area class from login form
-}
-
-
-function Removelogin(){
-    const taskTemplate = document.getElementById('login-form');
-    // add hide class to login form
-    taskTemplate.classList.add('area');// add area class to login form
-    taskTemplate.classList.remove('display-section');// remove display-section class from login form
+/* load sign up page */
+function LoadSingUPage(){
+    const taskTemplate = document.getElementById('signup-form');// get signup form
+    taskTemplate.classList.add('display-section');// add display class to signup form
+    taskTemplate.classList.remove('area');// remove hide class to signup form
 }
 
 
-
-function Myapp(){
-   
+/* remove sign in page */
+function RemoveSignInPage(){
+    const taskTemplate = document.getElementById('login-form');// get login form
+    taskTemplate.classList.add('area');// add hide class to login form
+    taskTemplate.classList.remove('display-section');// remove display class to login form
 }
+
 
 
 
@@ -66,10 +58,18 @@ function sign_in(event){
 
 
 
+
+
+
+
+
+
+
+
+
 function sign_up(){
    
 }
-
 
 
 

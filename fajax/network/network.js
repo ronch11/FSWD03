0bf51d138ea96c69229152a39934b715dc4d765b
ron hodadedi <hodadedi@g.jct.ac.il>
2023-03-15 16:23:12 +0200

    try to fix

--- fajax/network/network.js
+++ fajax/network/network.js
@@ -1,16 +1,23 @@
-
+import { FXMLhttpRequest } from './fxmlhttprequest.js';
+import { server } from './server/server.js'
 
 export class network {
     /**
      * send the reuest to the server.
-     * @param FXMLhttpRequest the request to the server
+     * @param {FXMLhttpRequest} the request to the server
      * @returns pass throw the callback function for the return
      */
-    send = (FXMLhttpRequest) => {
-        server.UseServer( FXMLhttpRequest, 
-            function (response){
-                FXMLhttpRequest.onReady(response)
-            }
-        )
+    static send(FXMLhttpRequest) {
+        console.log('network: ',FXMLhttpRequest.method, FXMLhttpRequest.url);
+        if (FXMLhttpRequest.url.length < 0) {
+            console.log('network: invalid url');
+            return;
+        }
+        if(FXMLhttpRequest.url.split('/')[0] === 'server.com'){
+            server.handle(FXMLhttpRequest, function (response){
+                console.log('network: responding to client with ', response)
+                FXMLhttpRequest.onready_handler(response);
+            })
+        }
     }
 }

d22170f451893bc528068eb4cf958fc33f7877a9
uditra <yehudaar@g.jct.ac.il>
2023-03-16 12:45:58 +0200

    change back

--- fajax/FXMLHttpRequest.js
+++ fajax/FXMLHttpRequest.js
@@ -1,28 +1,21 @@
-import { network } from './network.js';
-
-
-export class FXMLhttpRequest {
-
+class FXMLhttpRequest {
     /**
      * method to open request to the server through the network
      * @param {string} method the method whated to do
      * @param {string} url the url to do the method
      * @param {string} body the request body
-     * @param {Function} onready_handler the function to actived when to request is finished
+     * @param {Function} onReady the function to actived when to request is finished
      */
-    open(method, url, body, onready_handler) {
+    open = (method, url, body, onReady) => {
         this.method = method;
         this.url = url;
         this.body = body;
-        this.onready_handler = onready_handler;
-        this.status = 0;
+        this.onReady = onReady;
     }
-
     /**
      * send the method to the server through the network
      */
-    send() {
+    send = () => {
         network.send(this);
     }
-
 }
--- fajax/network/network.js
+++ fajax/network/network.js
@@ -1,23 +1,14 @@
-import { FXMLhttpRequest } from './fxmlhttprequest.js';
-import { server } from './server/server.js'
-
-export class network {
+class network {
     /**
      * send the reuest to the server.
-     * @param {FXMLhttpRequest} the request to the server
+     * @param FXMLhttpRequest the request to the server
      * @returns pass throw the callback function for the return
      */
-    static send(FXMLhttpRequest) {
-        console.log('network: ',FXMLhttpRequest.method, FXMLhttpRequest.url);
-        if (FXMLhttpRequest.url.length < 0) {
-            console.log('network: invalid url');
-            return;
-        }
-        if(FXMLhttpRequest.url.split('/')[0] === 'server.com'){
-            server.handle(FXMLhttpRequest, function (response){
-                console.log('network: responding to client with ', response)
-                FXMLhttpRequest.onready_handler(response);
-            })
-        }
+    send = (FXMLhttpRequest) => {
+        server.UseServer( FXMLhttpRequest, 
+            function (response){
+                FXMLhttpRequest.onReady(response)
+            }
+        )
     }
 }
